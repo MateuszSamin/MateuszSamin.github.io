e4a087cd0e547b69f0851896330a34385226cc4f
MateuszSamin <63960220+MateuszSamin@users.noreply.github.com>
2020-04-19 17:03:12 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+$("#omnie").on("click", function(){
+    $("body, html").animate({
+        scrollTop: 1000
+    }, 1000)
+})
+
+$("#kontakt").on("click", function(){
+    $("body, html").animate({
+        scrollTop: 4000
+    }, 2000)
+})
+
+$("#oferta").on("click", function(){
+    $("body, html").animate({
+        scrollTop: 1600
+    }, 2000)
+})
